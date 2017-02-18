754f97fcb5c529efd08091c8e9b53e731d94a89e
Jorge Zapata <jorgezapata@Jorges-MacBook-Pro.local>
2017-02-17 19:07:48 -0500

    exercise 1

---
+++ zombies.rb
@@ -0,0 +1,5 @@
+zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars", "shotgun", "compass", "CB radio", "Tactical 10mm"]
+
+zombie_apocalypse_supplies.each do | zombie_supply |
+  puts zombie_supply
+end
