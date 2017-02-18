dfd0f158d996f985255715f44396c216da29ba6f
Jorge Zapata <jorgezapata@Jorges-MacBook-Pro.local>
2017-02-17 20:05:12 -0500

    zombies2.rb

---
+++ zombies2.rb
@@ -0,0 +1,3 @@
+zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars", "shotgun", "compass", "CB radio", "Tactical 10mm"]
+
+  puts zombie_apocalypse_supplies.sort_by(&:downcase)
